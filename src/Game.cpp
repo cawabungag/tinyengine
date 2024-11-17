864c338daa105903d007a425be1bc6336d873405
Ilyas Ilyasov <38839317+cawabungag@users.noreply.github.com>
2024-11-17 19:10:33 +0300

    render sdl window

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,8 +1,10 @@
 #include "Game.h"
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 Game::Game() {
+    isRunning = false;
     std::cout << "Game constructor called!" << std::endl;    
 }
 
@@ -17,23 +19,56 @@ void Game::Initialize() {
         return;
     }
 
-    SDL_Window* window = SDL_CreateWindow("tinyengine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 720, SDL_WINDOW_BORDERLESS); 
+    SDL_DisplayMode displayMode;
+    SDL_GetCurrentDisplayMode(0, &displayMode);
+    windowWidth = 800;
+    windowHight = 600;
+    window = SDL_CreateWindow("tinyengine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHight, SDL_WINDOW_BORDERLESS); 
     if (!window)
     {
         std::cerr << "Error creating SDL window" << std :: endl;
         return;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (!renderer)
     {
         std::cerr << "Error creating SDL renderer" << std :: endl;
         return;
     }
+
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
+    isRunning = true;
+}
+
+void Game::Setup()
+{
+
 }
 
 void Game::ProcessInput() {
-
+    SDL_Event sdlEvent;
+    while (SDL_PollEvent(&sdlEvent))
+    {
+        switch (sdlEvent.type)
+        {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
+        
+        case SDL_KEYDOWN:
+            if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)
+            {
+                isRunning = false;
+            }
+            break;
+            
+        default:
+            break;
+        }
+    }
+    
 }
 
 void Game::Update() {
@@ -41,17 +76,27 @@ void Game::Update() {
 }
 
 void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+    SDL_RenderClear(renderer);
 
+    // SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    // SDL_Rect player = {10, 10, 20, 20};
+    // SDL_RenderFillRect(renderer, &player);
+
+
+    SDL_RenderPresent(renderer);
 }
 
 void Game::Run() {
-    // while (true) {
-    //     ProcessInput();
-    //     Update();
-    //     Render();
-    // }
+    while (isRunning) {
+        ProcessInput();
+        Update();
+        Render();
+    }
 }
 
 void Game::Destroy() {
-
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
